4bf4bbe65171657aa5e0e052e8f6c42e64407f59
Panchenko Denis <Pandenisko@gmail.com>
2025-04-23 10:19:00 +0300

    updated contact slice

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -3,18 +3,16 @@ const slice = createSlice({
     name: "contacts",
     initialState: {
         contacts:
-        [
-            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-        ]
+        {
+            items: [],
+        }
+
     },
         reducers: {
             addContact: (state, action) => {
                state.contacts.push(action.payload);
             },
-            removeContact: (state, action) => {
+            deleteContact: (state, action) => {
                 state.contacts = state.contacts.filter(contact => contact.id !== action.payload);
             }
     }
@@ -22,4 +20,4 @@ const slice = createSlice({
 
 export default slice.reducer;
 
-export const { addContact, removeContact} = slice.actions;
+export const { addContact, deleteContact} = slice.actions;
